7a559e3afca92aa49fe4912d6e2ad70dd9d03ef1
Naveen D B <ndb@kogentix.com>
2018-12-06 07:12:16 +0530

    Version1: About me

---
+++ src/app/app.module.ts
@@ -0,0 +1,39 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
+
+
+import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { TopMenuComponent } from './top-menu/top-menu.component';
+import { TopContentComponent } from './top-content/top-content.component';
+import { MyWorksComponent } from './my-works/my-works.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+
+const appRoutes: Routes = [
+  { path: 'dashboard', component:  DashboardComponent },
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: '**', redirectTo: '/dashboard', pathMatch: 'full' }
+]
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    TopMenuComponent,
+    TopContentComponent,
+    MyWorksComponent,
+    AboutMeComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(appRoutes, { useHash: true }),
+    BrowserAnimationsModule,
+    MaterialModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/my-works/my-works.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-my-works',
+  templateUrl: './my-works.component.html',
+  styleUrls: ['./my-works.component.scss'],
+  encapsulation: ViewEncapsulation.Emulated
+})
+export class MyWorksComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
