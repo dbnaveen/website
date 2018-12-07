5117e0fe281da0c1b6b4056cc31b090ceed7318b
Naveen D B <ndb@kogentix.com>
2018-12-07 15:44:40 +0530

    Version1: skills

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material';
+import {MatTabsModule} from '@angular/material/tabs';
 
 @NgModule({
-  imports: [MatButtonModule],
-  exports: [MatButtonModule],
+  imports: [MatButtonModule, MatTabsModule],
+  exports: [MatButtonModule, MatTabsModule],
 })
 export class MaterialModule { }
--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -1,9 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { viewParentEl } from '@angular/core/src/view/util';
 
 @Component({
   selector: 'app-skills',
   templateUrl: './skills.component.html',
-  styleUrls: ['./skills.component.scss']
+  styleUrls: ['./skills.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class SkillsComponent implements OnInit {
 
