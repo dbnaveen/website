5e2af831c2af1e81601dc78f6dd20671a296a729
Naveen D B <ndb@kogentix.com>
2018-12-20 16:57:43 +0530

    version1: carousal recommendations

--- src/app/testimonials/testimonials.component.ts
+++ src/app/testimonials/testimonials.component.ts
@@ -1,12 +1,14 @@
-import { Component, OnInit, ViewChild, ChangeDetectorRef } from '@angular/core';
+import { Component, OnInit, ViewChild, ChangeDetectorRef, ViewEncapsulation } from '@angular/core';
 import { NguCarousel, NguCarouselConfig } from '@ngu/carousel';
 
 @Component({
   selector: 'app-testimonials',
   templateUrl: './testimonials.component.html',
-  styleUrls: ['./testimonials.component.scss']
+  styleUrls: ['./testimonials.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class TestimonialsComponent implements OnInit {
+  public carouselTileItems: Array<any>;
   name = 'Angular';
   slideNo = 0;
   withAnim = true;
@@ -15,17 +17,35 @@ export class TestimonialsComponent implements OnInit {
   carouselConfig: NguCarouselConfig = {
     grid: { xs: 1, sm: 1, md: 1, lg: 1, all: 0 },
     load: 3,
-    interval: {timing: 4000, initialDelay: 1000},
+    // interval: {timing: 4000, initialDelay: 1000},
     loop: true,
     touch: true,
     velocity: 0.2
   }
-  carouselItems = [1, 2, 3];
+  carouselItems = [
+    {
+      id:'1',
+      title:'Jojo John Moolayil',
+      subTitle: 'Jojo John managed Naveen directly',
+      message:'Naveen is an extremely diligent and goal oriented individual. His passion for continuously learning and improving his skillsets'+
+      'and problem solving abilities makes him an extremely dependable team member.'+
+      'Naveen is technically sound and has rich exposure to a variety of technologies in UI development.'
+    }, 
+    {
+      id:'2',
+      title:'Darshith Badiyani',
+      subTitle: 'Naveen worked with Darshith in the same group',
+      message:'Naveen is one of the serious and hardworking person I have worked with at Flutura. He has great experience on the UI front'+
+      ' and is open to learn new things and pitch his own ideas for best user experience. He\'s a responsible developer willing to go the'+
+      'distance for best output.'
+    }, 
+  ];
 
   constructor(private cdr: ChangeDetectorRef) { }
 
 
   ngOnInit() {
+    this.carouselTileItems = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
     this.cdr.detectChanges();
   }
 
