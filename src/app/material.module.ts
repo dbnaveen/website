5345848658a8f7b1a4bbf6b74c34911538bcd63a
Naveen D B <ndb@kogentix.com>
2018-12-26 13:56:04 +0530

    version1: Footer

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material';
 import {MatTabsModule} from '@angular/material/tabs';
 import {MatDividerModule} from '@angular/material/divider';
+import {MatTooltipModule} from '@angular/material/tooltip';
 
 @NgModule({
-  imports: [MatButtonModule, MatTabsModule, MatDividerModule],
-  exports: [MatButtonModule, MatTabsModule, MatDividerModule]
+  imports: [MatButtonModule, MatTabsModule, MatDividerModule, MatTooltipModule],
+  exports: [MatButtonModule, MatTabsModule, MatDividerModule, MatTooltipModule]
 })
 export class MaterialModule { }
