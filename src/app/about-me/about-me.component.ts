e5beaeb6d571706efbe46b075bdb78bbbfc89602
Db <naveen.db@accenture.com>
2019-01-01 08:50:12 +0530

    CSS fixes

--- src/app/about-me/about-me.component.ts
+++ src/app/about-me/about-me.component.ts
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about-me.component.scss']
 })
 export class AboutMeComponent implements OnInit {
-
+  birthdate:any;
+  age:any;
   constructor() { }
 
   ngOnInit() {
+    var date = '10/12/1992';
+    this.birthdate = new Date(date);
+    var timeDiff = Math.abs(Date.now() - this.birthdate.getTime() );
+    this.age = Math.floor((timeDiff / (1000 * 3600 * 24)) / 365);
   }
 
 }
