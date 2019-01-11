9e34cb1a276ce60fc7ab9be633c2a62f94681cf6
Db <naveen.db@accenture.com>
2019-01-11 22:36:42 +0530

    version1 | works design change

--- src/app/top-content/top-content.component.ts
+++ src/app/top-content/top-content.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-top-content',
   templateUrl: './top-content.component.html',
-  styleUrls: ['./top-content.component.scss']
+  styleUrls: ['./top-content.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class TopContentComponent implements OnInit {
 
