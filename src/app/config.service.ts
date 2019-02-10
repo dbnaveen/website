634f74152b82bd30f27b14928a0fc8f01eb31020
Db <naveen.db@accenture.com>
2019-02-10 20:34:23 +0530

    version1| api on-hold

--- src/app/config.service.ts
+++ src/app/config.service.ts
@@ -9,15 +9,13 @@ export class ConfigService {
 
   constructor(private http: HttpClient) {
     this.configUrl = '/api/v1/';
-    this.configUrl = 'https://api.sparkpost.com/api/v1/';
   }
 
-  private setHeaders(headers: HttpHeaders ): object {
-      var token = '8a0552653fce7ad6e4af3b900d0a28e89d78c3a0';
-      headers = headers || new HttpHeaders();
-      headers = headers.append('Access-Control-Allow-Origin', '*')
-      headers = headers.append('Authorization', `${token}`)
-      return { headers };
+  private setHeaders(headers: HttpHeaders): object {
+    var token = '8a0552653fce7ad6e4af3b900d0a28e89d78c3a0';
+    headers = headers || new HttpHeaders();
+    headers = headers.append('Authorization', `${token}`)
+    return { headers };
   }
 
   postData(url, params, headers?: HttpHeaders | null) {
