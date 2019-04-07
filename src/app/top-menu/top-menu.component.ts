6c73e04a4df2e1acb5990cec2d8e9e8d553d507d
Db <naveen.db@accenture.com>
2019-04-07 18:52:15 +0530

    navbar link animation and burger animation

--- src/app/top-menu/top-menu.component.ts
+++ src/app/top-menu/top-menu.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-top-menu',
-  templateUrl: './top-menu.component.html',
-  styleUrls: ['./top-menu.component.scss']
+  selector: "app-top-menu",
+  templateUrl: "./top-menu.component.html",
+  styleUrls: ["./top-menu.component.scss"]
 })
 export class TopMenuComponent implements OnInit {
+  constructor() {}
+  navLinks: any;
 
-  constructor() { }
+  toggleBurger() {
+    const burger = document.querySelector(".burger");
+    this.navLinks = document.querySelectorAll(".navbar-nav button");
+    burger.classList.toggle("toggle");
 
-  ngOnInit() {
+    for (var i = 0; i < this.navLinks.length; i++) {
+      if (this.navLinks[i].style.animation) {
+        this.navLinks[i].style.animation = "";
+      } else {
+        this.navLinks[i].style.animation = `navLinkFade 0.5s ease backwards ${i / 5}s`;
+      }
+    }
   }
 
+  ngOnInit() {}
 }
