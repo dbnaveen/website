0b9af1a6b67cce9629190a0cb1ac7d77600b6e8b
Db <naveen.db@accenture.com>
2019-04-08 09:30:30 +0530

    version1 | typewriting effect

--- src/app/top-content/top-content.component.ts
+++ src/app/top-content/top-content.component.ts
@@ -10,7 +10,30 @@ export class TopContentComponent implements OnInit {
 
   constructor() { }
 
+  type () {
+    const texts = ['  Developer & Designer    '];
+    let count = 0;
+    let index = 0;
+    let currentText = '';
+    let letter = '';
+
+    (function type() {
+      if(count === texts.length) {
+        count = 0;
+      }
+      currentText = texts[count];
+      letter = currentText.slice(0, ++index);
+      document.querySelector('.typing').textContent = 'Curious Front-end' + letter;
+      if(letter.length === currentText.length) {
+        count++;
+        index = 0;
+      }
+      setTimeout(type, 250);
+    }());
+  }
+
   ngOnInit() {
+    this.type();
   }
 
 }
