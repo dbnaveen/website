b705d052a17de935fe6c05c596f62c7b799c7dd7
Db <naveen.db@accenture.com>
2019-05-10 19:08:27 +0530

    version1 | recommendation redirect

--- src/app/testimonials/testimonials.component.ts
+++ src/app/testimonials/testimonials.component.ts
@@ -1,19 +1,25 @@
-import { Component, OnInit, ViewChild, ChangeDetectorRef, ViewEncapsulation } from '@angular/core';
-import { NguCarousel, NguCarouselConfig } from '@ngu/carousel';
+import {
+  Component,
+  OnInit,
+  ViewChild,
+  ChangeDetectorRef,
+  ViewEncapsulation
+} from "@angular/core";
+import { NguCarousel, NguCarouselConfig } from "@ngu/carousel";
 
 @Component({
-  selector: 'app-testimonials',
-  templateUrl: './testimonials.component.html',
-  styleUrls: ['./testimonials.component.scss'],
+  selector: "app-testimonials",
+  templateUrl: "./testimonials.component.html",
+  styleUrls: ["./testimonials.component.scss"],
   encapsulation: ViewEncapsulation.None
 })
 export class TestimonialsComponent implements OnInit {
   public carouselTileItems: Array<any>;
-  name = 'Angular';
+  name = "Angular";
   slideNo = 0;
   withAnim = true;
   resetAnim = true;
-  @ViewChild('myCarousel') myCarousel: NguCarousel<any>;
+  @ViewChild("myCarousel") myCarousel: NguCarousel<any>;
   carouselConfig: NguCarouselConfig = {
     grid: { xs: 1, sm: 1, md: 1, lg: 1, all: 0 },
     load: 3,
@@ -21,28 +27,33 @@ export class TestimonialsComponent implements OnInit {
     loop: true,
     touch: true,
     velocity: 0.2
-  }
+  };
   carouselItems = [
     {
-      id:'1',
-      title:'Jojo John Moolayil',
-      subTitle: 'Jojo John managed Naveen directly',
-      message:'Naveen is an extremely diligent and goal oriented individual. His passion for continuously learning and improving his skillsets'+
-      'and problem solving abilities makes him an extremely dependable team member.'+
-      'Naveen is technically sound and has rich exposure to a variety of technologies in UI development.'
-    }, 
+      id: "1",
+      title: "Jojo John Moolayil",
+      subTitle: "Jojo John managed Naveen directly",
+      message:
+        "Naveen is an extremely diligent and goal oriented individual. His passion for continuously learning and improving his skillsets" +
+        "and problem solving abilities makes him an extremely dependable team member." +
+        "Naveen is technically sound and has rich exposure to a variety of technologies in UI development."
+    },
     {
-      id:'2',
-      title:'Darshith Badiyani',
-      subTitle: 'Naveen worked with Darshith in the same group',
-      message:'Naveen is one of the serious and hardworking person I have worked with at Flutura. He has great experience on the UI front '+
-      'and is open to learn new things and pitch his own ideas for best user experience. He\'s a responsible developer willing to go the '+
-      'distance for best output.'
-    }, 
+      id: "2",
+      title: "Darshith Badiyani",
+      subTitle: "Naveen worked with Darshith in the same group",
+      message:
+        "Naveen is one of the serious and hardworking person I have worked with at Flutura. He has great experience on the UI front " +
+        "and is open to learn new things and pitch his own ideas for best user experience. He's a responsible developer willing to go the " +
+        "distance for best output."
+    }
   ];
 
-  constructor(private cdr: ChangeDetectorRef) { }
+  constructor(private cdr: ChangeDetectorRef) {}
 
+  handleLinkedInClick(id) {
+    window.open("https://www.linkedin.com/in/naveen-d-b-0310a693/", "_blank");
+  }
 
   ngOnInit() {
     this.carouselTileItems = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
@@ -52,5 +63,4 @@ export class TestimonialsComponent implements OnInit {
   moveTo(slide) {
     this.myCarousel.moveTo(slide, !this.withAnim);
   }
-
 }
