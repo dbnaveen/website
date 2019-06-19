0513eacd7dff5a5dae7b9914dbcc968ee9083a80
Db <naveen.db@accenture.com>
2019-06-19 15:39:20 +0530

    versio2 | testimonials design change

--- src/app/testimonials/testimonials.component.ts
+++ src/app/testimonials/testimonials.component.ts
@@ -1,11 +1,9 @@
-import {
-  Component,
-  OnInit,
-  ViewChild,
-  ChangeDetectorRef,
-  ViewEncapsulation
-} from "@angular/core";
+import { Component, OnInit, ViewChild } from "@angular/core";
+import { ChangeDetectorRef, ViewEncapsulation } from "@angular/core";
 import { NguCarousel, NguCarouselConfig } from "@ngu/carousel";
+
+declare var require: any;
+const testimonialsJson = require("../../assets/json/testimonials.json");
 
 @Component({
   selector: "app-testimonials",
@@ -20,6 +18,9 @@ export class TestimonialsComponent implements OnInit {
   withAnim = true;
   resetAnim = true;
   @ViewChild("myCarousel") myCarousel: NguCarousel<any>;
+
+  constructor(private cdr: ChangeDetectorRef) {}
+
   carouselConfig: NguCarouselConfig = {
     grid: { xs: 1, sm: 1, md: 1, lg: 1, all: 0 },
     load: 3,
@@ -28,35 +29,14 @@ export class TestimonialsComponent implements OnInit {
     touch: true,
     velocity: 0.2
   };
-  carouselItems = [
-    {
-      id: "1",
-      title: "Jojo John Moolayil",
-      subTitle: "Jojo John managed Naveen directly",
-      message:
-        "Naveen is an extremely diligent and goal oriented individual. His passion for continuously learning and improving his skillsets" +
-        "and problem solving abilities makes him an extremely dependable team member." +
-        "Naveen is technically sound and has rich exposure to a variety of technologies in UI development."
-    },
-    {
-      id: "2",
-      title: "Darshith Badiyani",
-      subTitle: "Naveen worked with Darshith in the same group",
-      message:
-        "Naveen is one of the serious and hardworking person I have worked with at Flutura. He has great experience on the UI front " +
-        "and is open to learn new things and pitch his own ideas for best user experience. He's a responsible developer willing to go the " +
-        "distance for best output."
-    }
-  ];
 
-  constructor(private cdr: ChangeDetectorRef) {}
+  carouselItems = testimonialsJson;
 
-  handleLinkedInClick(id) {
+  handleLinkedInClick() {
     window.open("https://www.linkedin.com/in/naveen-d-b-0310a693/", "_blank");
   }
 
   ngOnInit() {
-    this.carouselTileItems = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
     this.cdr.detectChanges();
   }
 
